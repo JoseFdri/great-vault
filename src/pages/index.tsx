623180ae17087b1f18e2f32c8679cefbeb6b4809
Jose Rodriguez <josefernaguillen@gmail.com>
2023-01-07 15:15:35 -0500

    Feature/add linters (#1)
    * adding eslint and prettier

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,13 @@
-import { Header } from '../components/header';
-import { Footer } from '../components/footer';
+import { Header } from "../components/header";
+import { Footer } from "../components/footer";
 
-import styles from '../styles/Home.module.css'
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
     <div className={styles.container}>
-      <Header
-        title='My keys' 
-        description='Your favorite digital key holder' 
-        faviconUrl='/favicon.ico'/>
-      <Footer/>
+      <Header title="My keys" description="Your favorite digital key holder" faviconUrl="/favicon.ico" />
+      <Footer />
     </div>
-  )
+  );
 }
